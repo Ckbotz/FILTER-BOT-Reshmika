254af19ee763ba8b249de601da1bebf88ccc1d1d
kichu <kichuxpro@outlook.com>
2025-04-16 21:55:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -44,8 +44,8 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+DATABASE_NAME = environ.get('DATABASE_NAME', "PIRO")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'FILES')
 
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
